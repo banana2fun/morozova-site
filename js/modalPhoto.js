d6cd685ffec2882dbbf3e2b73e0791905bc3f504
Artyom Stepanov <banana2fun@gmail.com>
2018-07-22 16:49:11 +0700

    change style of modal vol.2

--- js/modalPhoto.js
+++ js/modalPhoto.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
         });
         $(document).click(function (e) {
             if ($(".modalContainer").is(e.target)) {
-                $(".modalContainer").hide();
+                $(".modalContainer").css('display', 'none');;
             }
         })
     });
